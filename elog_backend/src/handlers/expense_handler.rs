04ef9c330d84d798b4f7349967e83d50f932e401
Daniel Córdova A <danesc87@gmail.com>
2021-02-09 18:37:29 -0500

    Add only this month for getting expenses

--- elog_backend/src/handlers/expense_handler.rs
+++ elog_backend/src/handlers/expense_handler.rs
@@ -4,6 +4,8 @@ use actix_web::{
     get,
     post
 };
+
+use chrono::{Local, Datelike, NaiveDate, NaiveTime, NaiveDateTime};
 use crate::models::expense::{
     Expense,
     NewExpense
@@ -32,10 +34,15 @@ pub async fn insert_expense (
 pub async fn get_all_expenses(
     authenticated_request: AuthenticatedRequest
 ) -> Result<HttpResponse, ElogError> {
+    let current_date = Local::now().naive_local();
+    let beggining_date = NaiveDateTime::new(
+        NaiveDate::from_ymd(current_date.year(), current_date.month(), 1),
+        NaiveTime::from_hms_milli(0,0,0,0)
+    );
     Expense::get_all_expenses(
         &authenticated_request.connection,
         authenticated_request.user_id,
-        None
+        Some((beggining_date, current_date))
     ).map(|list| {
         HttpResponse::Ok().json(list)
     })
